0f176605d17035e503ed125fa9f3c976acd419ee
thomenat <156375450+thomenat@users.noreply.github.com>
2024-04-20 13:54:25 +0100

    improved UX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function displayPoem(response) {
         strings: response.data.answer,
         autoStart: true,
         cursor: null,
-        delay: 80,
+        delay: 40,
       });
   
       console.log("poem generated");
@@ -17,9 +17,14 @@ function generatePoem(event) {
     let apiKey = "deabbt600bd7ofd44dbd308802faa2f2";
     let userInstructions = document.querySelector("#instructions");
     let prompt = `Please write a poem about ${userInstructions.value}`;
-    let context = "You are poem expert and loves to write short poems. Please write a 4 line poem, dispaly each sentence in a line in basic HTML. Please make the poem funny";
+    let context = "You are a poem expert and loves to write short poems. Your mission is to generate a 4 sentences poem in HTML format. The three first sentences should be followed by a <br>. Make sure to follow the user instructions. Please make the poem funny. Display the poem directly without any other text before it. Do not add an extra line at the end of the poem";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = ` <div> 🔄 Generating your poem about ${userInstructions.value}</div>`;
+
+
     axios.get(apiUrl).then(displayPoem);
 
     console.log("generating poem");
