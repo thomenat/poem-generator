eecbb01de255e21200a24e1927d0c305387baf69
thomenat <156375450+thomenat@users.noreply.github.com>
2024-05-07 20:52:34 +0100

    added more context

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generatePoem(event) {
     let apiKey = "deabbt600bd7ofd44dbd308802faa2f2";
     let userInstructions = document.querySelector("#instructions");
     let prompt = `Please write a poem about ${userInstructions.value}`;
-    let context = "You are a poem expert and loves to write short poems. Your mission is to generate a 4 sentences poem in HTML format. The three first sentences should be followed by a <br>. Make sure to follow the user instructions. Please make the poem funny. Display the poem directly without any other text before it. Do not add an extra line at the end of the poem";
+    let context = "You are a poem expert and loves to write short poems. Your mission is to generate a 4 sentences poem in HTML format. The three first sentences should be followed by a <br>. Make sure to follow the user instructions. Please make the poem funny. Display the poem directly without any other text before it. Remove any extra lines and/or spaces at the end of the poem";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
     let poemElement = document.querySelector("#poem");
